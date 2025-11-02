7347ca68369c970ac5393263293a8ac74d3ff020
Vengadesan017 <vengadesang2003@gmail.com>
2025-11-02 16:03:52 +0530

    Stage 1.2 logger issue solved

--- Stage 1.2/logger.js
+++ Stage 1.2/logger.js
@@ -1,5 +1,10 @@
-// logger.js
 import winston from "winston";
+
+const filterByLevel = (level) => {
+  return winston.format((info) => {
+    return info.level === level ? info : false;
+  })();
+};
 
 const logger = winston.createLogger({
   level: "debug",
@@ -8,10 +13,38 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
   transports: [
-    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-    new winston.transports.File({ filename: "logs/debug.log", level: "warn" }),
-    new winston.transports.File({ filename: "logs/event.log", level: "info" })
-  ]
+    new winston.transports.File({
+      filename: "logs/error.log",
+      format: winston.format.combine(filterByLevel("error")),
+    }),
+    new winston.transports.File({
+      filename: "logs/debug.log",
+      format: winston.format.combine(filterByLevel("debug")),
+    }),
+    new winston.transports.File({
+      filename: "logs/event.log",
+      format: winston.format.combine(filterByLevel("info")),
+    }),
+  ],
 });
 
 export default logger;
+
+
+// // logger.js
+// import winston from "winston";
+
+// const logger = winston.createLogger({
+//   level: "debug",
+//   format: winston.format.combine(
+//     winston.format.timestamp(),
+//     winston.format.json()
+//   ),
+//   transports: [
+//     new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+//     new winston.transports.File({ filename: "logs/debug.log", level: "warn" }),
+//     new winston.transports.File({ filename: "logs/event.log", level: "info" })
+//   ]
+// });
+
+// export default logger;
